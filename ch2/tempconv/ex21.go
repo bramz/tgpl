15436d1fbbecb33fcdfd8dd80b086bcc10fea3da
bramz <brockramz@gmail.com>
2018-09-09 16:48:33 -0400

    KtoF and FtoK functions fix

--- ch2/tempconv/ex21.go
+++ ch2/tempconv/ex21.go
@@ -16,12 +16,12 @@ const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
-	ZeroK         Kelvin  = -273.27
+	OneK          Kelvin  = 273.27
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
-func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
 
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
@@ -29,5 +29,5 @@ func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 func CToK(c Celsius) Kelvin { return Kelvin(c + 273.27) }
 func KToC(k Kelvin) Celsius { return Celsius(k - 273.27) }
 
-func KToF(k Kelvin) Fahrenheit { return CToF(KToC()) }
-func FToK(f Fahrenheit) Kelvin { return CToF(KToC()) }
+func KToF(k Kelvin) Fahrenheit { return CToF(KToC(k)) }
+func FToK(f Fahrenheit) Kelvin { return CToK(CToF(f)) }
